cf32e9a6671a21e30467d051ca9771112f2fde86
Thiago Morgado <thiago_morgado@hotmail.com>
2025-09-27 02:16:13 -0300

    feat: create LoginPage

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,11 +3,25 @@
 import { useSession } from "next-auth/react"
 import { redirect } from "next/navigation"
 import React from "react"
+import Loading from "../components/loading"
 
 export default function Page() {
   const { data: session, status } = useSession()
 
-  if (session?.user?.role === "admin") redirect("/dashboard/admin")
+  // 1. Enquanto a sessão está sendo verificada, mostramos um loading.
+  if (status === "loading") {
+    return <Loading />
+  }
+
+  // 2. Se o status for "unauthenticated" (token expirou ou não logado), redireciona para o login.
+  if (status === "unauthenticated") {
+    redirect("/")
+  }
+
+  // 3. Se o usuário for admin, redireciona para a página de admin.
+  if (session?.user?.role === "admin") {
+    redirect("/dashboard/admin")
+  }
 
   return <div>Tela do usuário: {session?.user?.role}</div>
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,54 +5,11 @@ import { useState } from "react"
 import { useRouter } from "next/navigation"
 
 export default function LoginPage() {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const router = useRouter()
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault()
-
-    const res = await signIn("credentials", {
-      redirect: false,
-      username: email, // 👈 precisa bater com o que você configurou no authorize
-      password,
-    })
-
-    if (res?.ok) {
-      router.push("/dashboard")
-    } else {
-      alert("Credenciais inválidas")
-    }
-  }
-
   return (
-    <div className="flex h-screen items-center justify-center bg-gray-100">
-      <form
-        onSubmit={handleSubmit}
-        className="bg-white p-6 rounded-xl shadow-md w-80 space-y-4"
-      >
-        <h1 className="text-xl font-bold text-center">Login</h1>
-        <input
-          type="email"
-          placeholder="Email"
-          className="w-full border rounded px-3 py-2"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Senha"
-          className="w-full border rounded px-3 py-2"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button
-          type="submit"
-          className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700"
-        >
-          Entrar
-        </button>
-      </form>
+    <div className="w-screen h-screen bg-[url('/fundo.png')] bg-contain bg-no-repeat bg-left-top flex flex-row-reverse">
+      <div className="w-[55%] bg-[#181818]">
+        <h1 className="">Login</h1>
+      </div>
     </div>
   )
 }
